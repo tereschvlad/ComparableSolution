4c905330bded360fa45b558e940c1905d5ef5585
tereschvlad <117782342+tereschvlad@users.noreply.github.com>
2025-03-30 01:49:19 +0200

    add testing

--- ComparableLibraryTest/GeneralComparableTest.cs
+++ ComparableLibraryTest/GeneralComparableTest.cs
@@ -5,61 +5,111 @@ namespace ComparableLibraryTest
     public class GeneralComparableTest
     {
         [Fact]
-        public void CheckConstructCorrectHashForCompare()
+        public void HashSum_Should_Be_Different_For_Different_Values()
         {
-            var test1 = new TestClass
+            var obj1 = new SampleComparable
             {
-                Prop1 = 1,
-                Prop2 = "First Instance",
-                Prop3 = new string[] { "Item1", "Item2" },
-                Prop4 = new List<string> { "String1", "String2" },
-                Prop5 = "Additional Info",
-                TestClassSecond1 = new List<TestClassSecond>
-            {
-                new TestClassSecond
+                TextProperty = "Hello",
+                NumberProperty = 5,
+                Price = 199.99m,
+                IsEnabled = true,
+                Timestamp = new DateTime(2022, 12, 25),
+                Duration = TimeSpan.FromMinutes(90),
+                OptionalNumber = 8,
+                OrderedNames = new List<string> { "Alice", "Bob", "Charlie" },
+                UnorderedNames = new List<string> { "Charlie", "Alice", "Bob" },
+                NestedObject = new NestedComparable { NestedText = "Nested", NestedNumber = 42 },
+                AlternativeOrderList = new List<AlternativeComparable>()
                 {
-                    Prop1 = 10,
-                    Prop2 = "Second Instance Prop2",
-                    Prop3 = "Nested Instance 1"
-                }
-            },
-                TestClass2 = new TestClassSecond
-                {
-                    Prop1 = 20,
-                    Prop2 = "Another Prop2",
-                    Prop3 = "Nested Instance 2"
+                    new AlternativeComparable { CustomText = "Test", CustomNumber = 100 },
+                    new AlternativeComparable { CustomText = "Test2", CustomNumber = 101 }
                 },
-                Prop6 = new List<string> { "String1", "String2" }
-            };
-            var test2 = new TestClass
-            {
-                Prop1 = 1,
-                Prop2 = "First Instance",
-                Prop3 = new string[] { "Item1", "Item2" },
-                Prop4 = new List<string> { "String1", "String2" },
-                Prop5 = "Additional Info",
-                TestClassSecond1 = new List<TestClassSecond>
-            {
-                new TestClassSecond
-                {
-                    Prop1 = 10,
-                    Prop2 = "Second Instance Prop2",
-                    Prop3 = "Nested Instance 1"
-                }
-            },
-                TestClass2 = new TestClassSecond
-                {
-                    Prop1 = 20,
-                    Prop2 = "Another Prop2",
-                    Prop3 = "Nested Instance 2"
-                },
-                Prop6 = new List<string> { "String2", "String1" }
             };
 
-            var hash1 = test1.GetHashSum();
-            var hash2 = test2.GetHashSum();
+            var obj2 = new SampleComparable
+            {
+                TextProperty = "World",
+                NumberProperty = 10,
+                Price = 299.99m,
+                IsEnabled = false,
+                Timestamp = new DateTime(2023, 1, 1),
+                Duration = TimeSpan.FromMinutes(120),
+                OptionalNumber = null,
+                OrderedNames = new List<string> { "Eve", "Dan", "Frank" },
+                UnorderedNames = new List<string> { "Frank", "Eve", "Dan" },
+                NestedObject = new NestedComparable { NestedText = "NestedDiff", NestedNumber = 99 },
+                AlternativeOrderList = new List<AlternativeComparable>()
+                {
+                    new AlternativeComparable { CustomText = "Test3", CustomNumber = 102 },
+                    new AlternativeComparable { CustomText = "Test4", CustomNumber = 103 }
+                }
+            };
 
-            Assert.Equal(hash1, hash2);
+            Assert.NotEqual(obj1.HashSum, obj2.HashSum);
         }
+
+
+        [Fact]
+        public void HashSum_Should_Be_Same_For_Identical_Objects()
+        {
+            var obj1 = new SampleComparable
+            {
+                TextProperty = "Test",
+                NumberProperty = 15,
+                OrderedNames = new List<string> { "A", "B", "C" },
+                UnorderedNames = new List<string> { "X", "Y", "Z" },
+                NestedObject = new NestedComparable { NestedText = "Inside", NestedNumber = 123 },
+                AlternativeOrderList = new List<AlternativeComparable>()
+                {
+                    new AlternativeComparable { CustomText = "Test", CustomNumber = 100 },
+                    new AlternativeComparable { CustomText = "Test2", CustomNumber = 101 }
+                },
+                AlternativeUnorderList = new List<AlternativeComparable>()
+                {
+                    new AlternativeComparable { CustomText = "Test3", CustomNumber = 102 },
+                    new AlternativeComparable { CustomText = "Test4", CustomNumber = 103 }
+                }
+            };
+
+            var obj2 = new SampleComparable
+            {
+                TextProperty = "Test",
+                NumberProperty = 15,
+                OrderedNames = new List<string> { "A", "B", "C" },
+                UnorderedNames = new List<string> { "Z", "X", "Y" },
+                NestedObject = new NestedComparable { NestedText = "Inside", NestedNumber = 123 },
+                AlternativeOrderList = new List<AlternativeComparable>()
+                {
+                    new AlternativeComparable { CustomText = "Test", CustomNumber = 100 },
+                    new AlternativeComparable { CustomText = "Test2", CustomNumber = 101 }
+                },
+                AlternativeUnorderList = new List<AlternativeComparable>()
+                {
+                    new AlternativeComparable { CustomText = "Test4", CustomNumber = 103 },
+                    new AlternativeComparable { CustomText = "Test3", CustomNumber = 102 }
+                }
+            };
+
+            Assert.Equal(obj1.HashSum, obj2.HashSum);
+        }
+
+        [Fact]
+        public void HashSum_Should_Ignore_Order_For_Unordered_Collections()
+        {
+            var obj1 = new SampleComparable { UnorderedNames = new List<string> { "A", "B", "C" } };
+            var obj2 = new SampleComparable { UnorderedNames = new List<string> { "C", "B", "A" } };
+
+            Assert.Equal(obj1.HashSum, obj2.HashSum);
+        }
+
+        [Fact]
+        public void HashSum_Should_Compare_For_Similar_But_Different_Classes()
+        {
+            var obj1 = new NestedComparable { NestedText = "Test", NestedNumber = 100 };
+            var obj2 = new AlternativeComparable { CustomText = "Test", CustomNumber = 100 };
+
+            Assert.Equal(obj1.HashSum, obj2.HashSum);
+        }
+
     }
 }
---
+++ ComparableLibraryTest/SampleComparable.cs
@@ -0,0 +1,64 @@
+﻿using ComparableLibrary;
+
+namespace ComparableLibraryTest
+{
+    public class SampleComparable : GeneralComparable
+    {
+        [ComparableProperty(2)]
+        public string TextProperty { get; set; }
+
+        [ComparableProperty(1)]
+        public int NumberProperty { get; set; }
+
+        [ComparableProperty(4)]
+        public decimal Price { get; set; }
+
+        [ComparableProperty(3)]
+        public bool IsEnabled { get; set; }
+
+        [ComparableProperty(6)]
+        public DateTime Timestamp { get; set; }
+
+        [ComparableProperty(5)]
+        public TimeSpan Duration { get; set; }
+
+        [ComparableProperty(7)]
+        public int? OptionalNumber { get; set; }
+
+        [ComparableProperty(9, type: ComparableCollectionType.Unordered)]
+        public List<string> UnorderedNames { get; set; }
+
+        [ComparableProperty(8)]
+        public List<string> OrderedNames { get; set; }
+
+        [ComparableProperty(10)]
+        public NestedComparable NestedObject { get; set; }
+
+        public double DoubleProperty { get; set; }
+        public string SimpleText { get; set; }
+
+        [ComparableProperty(type: ComparableCollectionType.Unordered)]
+        public List<AlternativeComparable> AlternativeOrderList { get; set; }
+
+        [ComparableProperty(type: ComparableCollectionType.Unordered)]
+        public List<AlternativeComparable> AlternativeUnorderList { get; set; }
+    }
+
+    public class NestedComparable : GeneralComparable
+    {
+        [ComparableProperty(2)]
+        public int NestedNumber { get; set; }
+
+        [ComparableProperty(1)]
+        public string NestedText { get; set; }
+    }
+
+    public class AlternativeComparable : GeneralComparable
+    {
+        [ComparableProperty(2, "NestedNumber")]
+        public int CustomNumber { get; set; }
+
+        [ComparableProperty(1, "NestedText")]
+        public string CustomText { get; set; }
+    }
+}
